1fd74c7331b6e45c2165df1700876b9213b7d2eb
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-04-01 21:32:08 +0530

    Task-Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Modals from './Components/FunctionCom/Modals';
+import CustomNavBar from './Components/FunctionCom/Styles/Button';
+import PopUp from './Components/FunctionCom/Styles/alert';
+import CustomFooter from './Components/FunctionCom/Styles/Footer';
+import ButtonUsage from './Components/MUi/Crads';
+
+
 
 function App() {
   return (
     <div className="App">
- 
+      
+
+      <CustomNavBar/>
+      
+      {true ? <Modals/> : null}
+
+      <PopUp/>
+     
+      <ButtonUsage/><br/><br/>
+
+      <CustomFooter/>
+
     </div>
   );
 }
---
+++ src/Components/FunctionCom/Styles/Footer.jsx
@@ -0,0 +1,12 @@
+
+const CustomFooter = ()=>{
+    return(
+        <div className="footerForPage">
+
+        <h5>&copy;All rights reserved by me!!</h5>
+        
+        </div>
+    )
+}
+
+export default CustomFooter
