32ab05c413ab8da389dd00b093aa2f7d8bd77bc3
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-04-04 13:57:09 +0530

    add

---
+++ src/Axios/getData.jsx
@@ -0,0 +1,56 @@
+import { Component } from "react";
+import axios from 'axios'
+import './Axios.css'
+
+class GetData extends Component{
+
+ state = {
+  products:[]
+ }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  fetchData = async()=>{
+
+    const result = await axios.get('https://dummyjson.com/products')
+    console.log(result);
+
+    this.setState({
+      products : result.data.products
+    })
+
+  }
+
+  render(){
+    console.log(this.state.products,'log from render');
+    return(
+      <div className="container">
+      <h1 id="products-Heading">Products</h1>
+      {
+        this.state.products.length>0
+        ?
+        <div className="card-details">
+        {
+          this.state.products.map(eachObject=>{
+            const {title,description,thumbnail,price} = eachObject
+            return(
+              <div className="card">
+                <img src={thumbnail} height={250} width={200} alt="" />
+              <h4>{title}</h4>
+              <p style={{fontSize:'15px'}}>{description}</p>
+              <button>${price}</button>
+              </div>
+            )
+          })
+        }
+        </div>
+        :
+        <h2>No products</h2>
+      }
+      </div>
+    )
+  }
+}
+export default GetData
