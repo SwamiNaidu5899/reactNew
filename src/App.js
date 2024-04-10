0d62aa83abe83e1db22bc88f3b15384b23e12815
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-04-10 18:42:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import Btn from './Props/button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Btn text='SignIn'/>
+      <Btn text='LogIn'/>
+      <Btn text='Magic'/>
     </div>
   );
 }
---
+++ src/Props/button.jsx
@@ -0,0 +1,11 @@
+
+const Btn = (props)=>{
+    const {text} = props
+    return(
+        <>
+        <button>{text}</button>
+        </>
+    )
+}
+
+export default Btn
