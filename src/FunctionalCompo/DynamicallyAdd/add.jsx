9ece33f243be9c5073ead08d1586f03524ab7233
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-04-12 10:30:34 +0530

    add

--- src/FunctionalCompo/DynamicallyAdd/add.jsx
+++ src/FunctionalCompo/DynamicallyAdd/add.jsx
@@ -1,4 +1,4 @@
-import './add.css'
+import "./add.css";
 import { useRef } from "react";
 import { useState } from "react";
 
@@ -9,54 +9,60 @@ const CrudOnAvng = () => {
 
   const addAvenger = () => {
     const inpEl = inpElRef.current.value;
-   if((inpEl) !=='' ) {  const newAvengers = [...avengers, inpEl];
-   setAvengers(newAvengers)
-   }
-    else 
-     alert("input field should be required dude..")
+    if (inpEl !== "") {
+      const newAvengers = [...avengers, inpEl];
+      setAvengers(newAvengers);
+    } else alert("input field should be required dude..");
   };
 
   const delAvg = (index) => {
-    const newAvengers = avengers.filter((eachAvng, i) => i !== index)
-    setAvengers(newAvengers)
-  }
+    const newAvengers = avengers.filter((eachAvng, i) => i !== index);
+    setAvengers(newAvengers);
+  };
 
   const upAvg = (index) => {
-    const ele = window.confirm('Do you want to Update')
+    const ele = window.confirm("Do you want to Update");
 
-    if(ele===true){
-      const  cnfElref = window.prompt('Enter')
+    if (ele === true) {
+      const cnfElref = window.prompt("Enter");
       // const inpEl = inpElRef.current.value;
       // const cnfEl = cnfElref.value
       const newAvengers = avengers.map((eachAvng, i) => {
-        if (i === index)
-          return cnfElref
-        else return eachAvng
-      })
-      setAvengers(newAvengers)
+        if (i === index) return cnfElref;
+        else return eachAvng;
+      });
+      setAvengers(newAvengers);
     }
-    }
+  };
 
-  inpElRef.current.value=''
+  inpElRef.current.value = "";
 
   return (
     <div className="container">
       <form action="">
-        <input type="text" ref={inpElRef} name="" id="" placeholder='Add or Update'/>
+        <input
+          type="text"
+          ref={inpElRef}
+          name=""
+          id=""
+          placeholder="Add or Update"
+        />
       </form>
-      <button id='btn' onClick={addAvenger}>Add</button>
+      <button id="btn" onClick={addAvenger}>
+        Add
+      </button>
       <div className="divCards">
-      {avengers.map((eachAvng, i) => {
-        return (
-          <div className="card" key={i}>
-            <h2>{eachAvng}</h2>
-            <div>
-              <button onClick={() => delAvg(i)}>Delete</button>
-              <button onClick={() => upAvg(i)}>Update</button>
+        {avengers.map((eachAvng, i) => {
+          return (
+            <div className="card" key={i}>
+              <h2>{eachAvng}</h2>
+              <div>
+                <button onClick={() => delAvg(i)}>Delete</button>
+                <button onClick={() => upAvg(i)}>Update</button>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       </div>
     </div>
   );
