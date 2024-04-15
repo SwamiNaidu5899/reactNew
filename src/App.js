40ce67d4642bcd16a75859ec0e891a083efe8569
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-04-15 08:52:33 +0530

    incmplt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Btn from './Props/button'
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page from './Router/Page.jsx'
+import Nav from './Router/nav.js'
+import About from './Router/about.js' 
+import Contact from './Router/contact.jsx'
 
 function App() {
   return (
     <div className="App">
-
+ <BrowserRouter>
+ <Routes>
+      <Route path='/' element={<Nav/>}/>
+      <Route path='/Page' element={<Page/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>      
+      </Routes>
+ </BrowserRouter>
     </div>
   );
 }
---
+++ src/Router/Page.jsx
@@ -0,0 +1,11 @@
+import './page.css'
+const Page = ()=>{
+    return(
+        <div id="container">
+            <>
+            <h1>Home</h1>
+            </>
+        </div>
+    )
+}
+export default Page
